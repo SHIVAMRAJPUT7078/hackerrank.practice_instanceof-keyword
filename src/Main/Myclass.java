59bdf9a6290abfa46bf1bd5c8c7ef5b33da0928b
shivam rajput <shivam.rajput_cs18@gmail.com>
2019-09-16 17:53:29 +0530

    add statements for program

--- src/Main/Myclass.java
+++ src/Main/Myclass.java
@@ -15,16 +15,18 @@ public class Myclass {
             scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
             scanner.close();
+
             if ((N % 2 != 0)||(N>20 )) {
                 System.out.println("Not Weird");
             }
+
             else if ((N % 2 == 0)& (N>=6 ) & (N<=20)){
                 System.out.println("Weird");
                 }
+
             else{
                 System.out.println("Not Weird");
                 }
-
 
             }
         }
